156518e8220129901aa6d5e7105d509cdbed8653
Kanjinaik <kanjinaik1234@gmail.com>
2025-01-27 10:28:22 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,15 @@
+
+#  write a program to input total marks of two students
+# and find difference in total of first and second student
+#total_marks1 = int(input("enter total marks of first student"))
+#total_marks2 = int(input("enter the total marks of second student"))
+#diff = total_marks1 - total_marks2
+#print("The total difference",total_marks1,"and", total_marks2,"is",diff)
+
+# write a program to input name,3 subject marks and calculate total
+name = input("enter the name")
+sub1 = int(input("enter the sub1 marks"))
+sub2 = int(input("enter the sub2 marks"))
+sub3 = int(input("enter the sub3 marks"))
+total_marks = sub1 + sub2 + sub3
+print(total_marks)
