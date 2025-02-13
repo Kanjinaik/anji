5b5315b54df243469b5fadc297e45d95660879db
Kanjinaik <kanjinaik1234@gmail.com>
2025-02-13 15:48:04 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Room,Customer,Booking
+
 # Register your models here.
-
-admin.site.register(Room)
-admin.site.register(Customer)
-admin.site.register(Booking)
-
--- urls.py
+++ urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .import views
+from . import views
 
-urlpatterns=[
-
-    path('available/',views.available_rooms),
-    path('create/',views.create_customer),
-    path('book/',views.book_rooms),
+urlpatterns = [
+    path('', views.home, name='home'),
 ]
